21ba1f13e019d52aa39224f225e6b241b5e3937d
Farhad100375 <farhad77771@hotmail.com>
2025-07-20 12:57:10 +0600

    ল.সা.গু.

---
+++ Gcd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Gcd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int i = (a < b) ? a : b;
+        while (i > 0) {
+            if (a % i == 0 && b % i == 0) {
+                System.out.println("GCD = " + i);
+                break;
+                }
+            i--; 
+        }
+    }
+}
